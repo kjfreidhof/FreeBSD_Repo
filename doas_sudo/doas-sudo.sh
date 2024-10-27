48fd08737a3c21ba934b8fc18bfc0c40d70ec919
Kyle Freidhof <freidhofkyle@proton.me>
2024-10-27 13:24:06 -0400

    Adding doas sudo install script for freebsd

---
+++ doas_sudo/doas-sudo.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env sh 
+Path_pkg="/usr/sbin/pkg"
+install_doas() {
+    if [ -f "$Path_pkg" ]; then
+        yes | pkg install doas
+        exit 0
+
+    else 
+            exit 1 
+
+
+    fi
+}
+
+install_sudo() {
+    if [ -f "$Path_pkg" ]; then
+        yes | pkg install sudo
+        exit 0
+
+    else 
+            exit 1 
+
+    fi
+}
+
+while true; do
+    printf "Do you want to install doas or sudo [d/s] \n"
+    read -r option
+
+    case "$option" in 
+        d)
+            install_doas
+            ;;
+
+        s)
+            install_sudo
+            ;;
+
+        *)
+            printf "Error: Choose either d or s \n"
+            exit 1
+            ;;
+
+            
+
+
+
+    esac
+
+
+
+
+done 
